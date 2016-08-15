cf534ba886ef576b25e89674b21dc388493a96bf
Merve Gungor <mrvgngr.0@gmail.com>
2016-08-15 15:25:57 +0300

    I did like buttons.

--- app/LoginPages/LoginPages.component.js
+++
@@ -1,9 +0,0 @@
-import loginPagesTemplate from 'text!./loginPages.html';
-import LoginPagesController from './loginPages.controller';
-
-const LoginComponent = {
-  template: loginPagesTemplate,
-  controller: LoginPagesController
-};
-
-export default LoginPagesComponent;
